93262c78a9d456bbb6d9ddf51323f29886980e9e
Rohith <rohith2210194@ssn.edu.in>
2024-12-27 11:38:01 +0530

    Refactor Load logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Canvas } from '@react-three/fiber';
-import { Html, useProgress } from '@react-three/drei';
-import App from '@/App.jsx';
-import '@/index.scss';
-import UI from '@/UI/UI.tsx';
-import Load from '@/Loader.tsx';
-
-
-function Loader() {
-  const { progress } = useProgress();
-  return (
-    <Html center>
-      <Load progress={progress}/>
-    </Html>   
-  );
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { Html, useProgress } from "@react-three/drei";
+import App from "@/App.jsx";
+import "@/index.scss";
+import UI from "@/UI/UI.tsx";
+import Load from "@/Loader.tsx";
 
 
 function CanvasWrapper() {
@@ -25,7 +15,13 @@ function CanvasWrapper() {
     <div id="container">
       {progress >= 100 && <UI />}
       <Canvas camera={{ fov: 45 }} shadows>
-        <React.Suspense fallback={<Loader />}>
+        <React.Suspense
+          fallback={
+            <Html center>
+              <Load progress={progress} />
+            </Html>
+          }
+        >
           <App />
         </React.Suspense>
       </Canvas>
@@ -33,9 +29,8 @@ function CanvasWrapper() {
   );
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CanvasWrapper />
   </React.StrictMode>
 );
-
