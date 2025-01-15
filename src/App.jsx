d3d45cf8a4ba88e6c5ae63edb96eec2f02c84bbe
Rohith <rohith2210194@ssn.edu.in>
2025-01-15 14:50:10 +0530

    Disable pointer temp

--- src/App.jsx
+++ src/App.jsx
@@ -72,7 +72,7 @@ export const App = () => {
 
   return (
     <>
-      {!isMobile && (
+      {!isMobile && false (
         <PointerLockControls
           onLock={pointerLockControlsLockHandler}
           onUnlock={pointerLockControlsUnlockHandler}
