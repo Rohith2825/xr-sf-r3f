704c40b5a083fda956345cd3b9e74f90aaf2a230
Rohith2825 <rohith2210194@ssn.edu.in>
2025-03-17 13:23:00 +0530

    New Loader

--- src/Loader.tsx
+++ src/Loader.tsx
@@ -7,35 +7,24 @@ interface LoadProps {
 }
 
 const Load: React.FC<LoadProps> = ({ progress }) => {
-
   const prevProgressRef = useRef(0);
-
   const displayProgress = Math.max(progress, prevProgressRef.current);
-
   prevProgressRef.current = displayProgress;
 
-
   return (
-    <div
-      className="loader-background"
-    >
+    <div className="loader-background">
       <div className="loader-container-container">
         <div className="loader-container" id="loaderContainer">
-          <div className="spinner">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+          <div className="snapchat-logo">
+            <img src="trace.png" alt="Snapchat Logo Trace" className="ghost-outline" />
           </div>
           <div className="loading-text-container">
-            <div className="loading-text typewriter">Delta XR</div>
-            <div className="loading-text">{Math.round(displayProgress)}%</div>
+            <span className="loading-text typewriter">Delta XR</span>
+            <span className="loading-text">{Math.round(displayProgress)}%</span>
           </div>
           <img
             id="powered-by-loader"
-            src="logo.avif"
+            src="icon_black.png"
             alt="Powered By Strategy Fox"
             className="powered-by-loader"
           />
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import { useProgress } from "@react-three/drei";
@@ -7,14 +7,12 @@ import "@/index.scss";
 import UI from "@/UI/UI.tsx";
 import { ProductService } from "./api/shopifyAPIService";
 import { useComponentStore } from "./stores/ZustandStores";
+import Load from "./Loader";
+
 
 function CanvasWrapper() {
   const { setProducts } = useComponentStore();
   const { progress } = useProgress();
-  const [videoLoaded, setVideoLoaded] = useState(false);
-  const videoRef = useRef(null);
-  const [maxProgress, setMaxProgress] = useState(0); 
-  const isMobile = /Mobi|Android/i.test(navigator.userAgent);
 
   async function fetchProducts() {
     try {
@@ -31,12 +29,12 @@ function CanvasWrapper() {
   useEffect(() => {
     const scrollY = window.scrollY;
     const joystickZone = document.getElementById("joystickZone");
-  
-    if (!(progress >= 100 && videoLoaded)) {
+
+    if (!(progress >= 100)) {
       if (joystickZone) {
         joystickZone.style.display = "none";
       }
-  
+
       document.body.style.position = "fixed";
       document.body.style.top = `-${scrollY}px`;
       document.body.style.width = "100%";
@@ -46,7 +44,7 @@ function CanvasWrapper() {
       if (joystickZone) {
         joystickZone.style.display = "block";
       }
-  
+
       document.body.style.position = "";
       document.body.style.top = "";
       document.body.style.width = "";
@@ -54,12 +52,12 @@ function CanvasWrapper() {
       document.body.style.touchAction = "";
       window.scrollTo(0, scrollY);
     }
-  
+
     return () => {
       if (joystickZone) {
         joystickZone.style.display = "block";
       }
-  
+
       document.body.style.position = "";
       document.body.style.top = "";
       document.body.style.width = "";
@@ -67,49 +65,19 @@ function CanvasWrapper() {
       document.body.style.touchAction = "";
       window.scrollTo(0, scrollY);
     };
-  }, [progress, videoLoaded]);
-
-  useEffect(() => {
-    // Update maxProgress only if progress exceeds the current maxProgress
-    if (progress > maxProgress) {
-      setMaxProgress(progress);
-    }
   }, [progress]);
-  
+
+
+
 
   return (
     <div id="container">
-      {progress >= 100 && videoLoaded ? (
-        <UI />
-      ) : (
-        <div className="video-loader">
-          <video
-            ref={videoRef}
-            src={isMobile ? "/media/Intro.mp4" : "/media/Intro.MOV"}
-            autoPlay
-            muted
-            playsInline
-            onEnded={() => setVideoLoaded(true)} // Set videoLoaded to true when the video ends
-          />
-          {/* Show progress bar and text only after the video finishes */}
-          {videoLoaded && (
-            <>
-              <div className="loading-text">Your experience is loading!</div>
-              <div className="progress-bar">
-                <div
-                  className="progress-bar-inner"
-                  style={{ width: `${Math.min(maxProgress, 100)}%` }}
-                />
-              </div>
-            </>
-          )}
-        </div>
-      )}
-      <Canvas camera={{ fov: 45 }} shadows>
-        <React.Suspense fallback={null}>
+      {progress >= 100 && <UI />}
+      <Suspense fallback={<Load progress={progress} />}>
+        <Canvas camera={{ fov: 45 }} shadows>
           <App />
-        </React.Suspense>
-      </Canvas>
+        </Canvas>
+      </Suspense>
     </div>
   );
 }
