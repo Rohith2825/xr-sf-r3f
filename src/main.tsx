991a812c565762d62a802beb020a8bd4788f866a
Rohith2825 <rohith2210194@ssn.edu.in>
2025-06-10 08:54:00 +0530

    inc fov

--- src/main.tsx
+++ src/main.tsx
@@ -53,7 +53,7 @@ function CanvasWrapper() {
   return (
     <div id="container">
       <Canvas 
-        camera={{ fov: 45 }} 
+        camera={{ fov: 75 }} 
         shadows
         dpr={[1, 2]}
         gl={{
